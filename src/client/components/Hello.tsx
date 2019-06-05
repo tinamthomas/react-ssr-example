b365c90d04df1516d602ae3c29a4967012aba0a7
TT <tinainink@gmail.com>
2019-06-05 09:17:12 +0530

    Tina | Add button for state changes

--- src/client/components/Hello.tsx
+++ src/client/components/Hello.tsx
@@ -4,10 +4,28 @@ interface IHelloProps {
     name: string;
 };
 
-export default class Hello extends React.Component<IHelloProps> {
+export default class Hello extends React.Component<IHelloProps, IHelloProps> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name
+        };
+        this.updateName = this.updateName.bind(this);
+    }
+    updateName(){
+        const actualName = this.props.name;
+        const alternateName = 'Mr. Neo';
+        const newName = this.state.name === actualName ? alternateName : actualName;
+
+        this.setState({name: newName});
+    }
+
     render() {
         return <div>
-            Hello {this.props.name}!
+            Hello {this.state.name}!
+            <button onClick={this.updateName}>
+                Click me!
+            </button>
         </div>
     }
 }
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import * as express from 'express';
+import * as path from 'path';
 import { renderToString } from 'react-dom/server';
 import Main from './../client/components/Main';
 
 import template from './template.index';
 
 const app = express();
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
-app.use('/', function (req, res) {
+app.use('/ssr', function (req, res) {
     const body = renderToString(<Main />);
     const html = template(body);
     res.send(html);
