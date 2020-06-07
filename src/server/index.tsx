f6c100436b0a1ad7994602627df2e2f16e5203bf
Tina Maria Thomas <tinainink@gmail.com>
2020-06-06 23:09:18 -0400

    Tina | React - SSR

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
 import * as express from 'express';
 import * as path from 'path';
-
+import * as fs from 'fs';
+import Main from './../client/components/Main';
 const app = express();
-app.use('/', express.static(path.join(__dirname, '../../dist')));
+// app.use('/', express.static(path.join(__dirname, '../../dist')));
+import * as ReactDOMServer from 'react-dom/server'
 
 
 app.get('/hello', function (req, res) {
     res.send('Hello World')
+});
+
+app.get('/', (req, res) => {
+    fs.readFile(path.resolve('./dist/index.html'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).send('An error occurred')
+        }
+        return res.send(
+          data.replace(
+            '<div id="example"></div>',
+            `<div id="example">${ReactDOMServer.renderToString(<Main />)}</div>`
+          )
+        )
+      })
 });
 
 
